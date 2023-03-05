0faff6e8fe16dfe1be840d4635fc0fbbdefde35f
Argetim Jusufi <argetimjusufi01@gmail.com>
2023-03-05 11:04:39 +0100

    refactor: ♻️ Some refactoring
    Converted indentations to spaces

--- app.js
+++ app.js
@@ -2,59 +2,65 @@ const process = require('process');
 const args = require('minimist')(process.argv.slice(2));
 
 const startDate = new Date(
-	args.start.split("/").reverse().join('/')
+  args.start.split("/").reverse().join('/')
 );
 
 const endDate = new Date(
-	args.end.split("/").reverse().join('/')
+  args.end.split("/").reverse().join('/')
 );
 
-console.log("start: ", startDate);
-console.log("end: ", endDate);
+const INVALID_DATE = "Invalid Date";
+
+if (startDate.toString() === INVALID_DATE || 
+    endDate.toString() === INVALID_DATE)
+{
+  console.log("usage: node app --start=[valid-date] --end=[valid-date]");
+  process.exit(-1)
+} 
 
 (function () {
 
-	const TWO_WEEKS = 14; // days
+  const TWO_WEEKS = 14; // days
 
-	function plusDays(date, days) {
-		const result = new Date(date);
-		result.setDate(result.getDate() + days);
-		return result;
-	}
+  function plusDays(date, days) {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
 
-	// to use correctly google calendar is raccomanded to 
-	// write events in blocks of 2 weeks with a pause 
-	function processRangeOfDates(start, end, counter) {
-		const currStartDate = plusDays(start, counter * TWO_WEEKS);
-		
-		const tempEndDate = plusDays(currStartDate, TWO_WEEKS);
-		const currEndDate = end < tempEndDate
-			? end
-			: tempEndDate;
-		
-		return [currStartDate, currEndDate, counter + 1];
-	}
+  // to use correctly google calendar is raccomanded to 
+  // write events in blocks of 2 weeks with a pause 
+  function processRangeOfDates(start, end, counter) {
+    const currStartDate = plusDays(start, counter * TWO_WEEKS);
+    
+    const tempEndDate = plusDays(currStartDate, TWO_WEEKS);
+    const currEndDate = end < tempEndDate
+      ? end
+      : tempEndDate;
+    
+    return [currStartDate, currEndDate, counter + 1];
+  }
 
-	// first range process
-	let [currStartDate, currEndDate, counter] = processRangeOfDates(
-		startDate, 
-		endDate,
-		0
-	);
+  // first range process
+  let [currStartDate, currEndDate, counter] = processRangeOfDates(
+    startDate, 
+    endDate,
+    0
+  );
 
-	while (currStartDate < currEndDate) {
-		processRangeOnCalendar(currStartDate, currEndDate);
+  while (currStartDate < currEndDate) {
+    processRangeOnCalendar(currStartDate, currEndDate);
 
-		// sliding of the range
-		[currStartDate, currEndDate, counter] = processRangeOfDates(
-			startDate, 
-			endDate,
-			counter
-		);
-	}
+    // sliding of the range
+    [currStartDate, currEndDate, counter] = processRangeOfDates(
+      startDate, 
+      endDate,
+      counter
+    );
+  }
 
 })()
 
 function processRangeOnCalendar(currStartDate, currEndDate) {
-	console.log(currStartDate, currEndDate);
+  console.log(currStartDate, currEndDate);
 }
